351a6cdc8c612b5722afd1d9271e72fa52362a0c
awaddd <awadini97@gmail.com>
2020-04-13 06:49:27 +0100

    refactored login and register background images to be responsive

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,35 +1,28 @@
 <template>
-  <div class="">
-    <section class="welcome-content is-fullheight">
-      <img class="welcome-content-image is-hidden-mobile" src="../assets/Flat-Mountains.svg" width="100%">
-      <!-- Got this svg background from https://www.svgbackgrounds.com/#flat-mountains -->
+  <!-- Got this svg background from https://www.svgbackgrounds.com/#flat-mountains -->
 
-      <!-- <span class="welcome-content-shade is-hidden-mobile"></span> -->
-      <div class="hero-body my-login-hero">
-        
-        <div class="container">
+  <div class="my-login">
 
-          <section class="my-form-wrapper container">
-            <ValidationObserver ref="form" v-slot="{ handleSubmit }">
-            <form class="my-form container" @keyup.enter.prevent="handleSubmit(login)">
+    <div class="container">
+      <section class="my-form-wrapper container">
+        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+        <form class="my-form container" @keyup.enter.prevent="handleSubmit(login)">
 
-              <p class="title is-size-4 has-text-primary has-text-centered"><strong>Login</strong></p>
+          <p class="title is-size-4 has-text-primary has-text-centered"><strong>Login</strong></p>
 
-              <BInputWithValidation vid="email" rules="required|email" type="email" icon="email" placeholder="John.smith@mail.com" label="Email" v-model="email"/>
+          <BInputWithValidation vid="email" rules="required|email" type="email" icon="email" placeholder="John.smith@mail.com" label="Email" v-model="email"/>
 
-              <BInputWithValidation vid="password" rules="required" type="password" icon="lock" label="Password"v-model="password" password-reveal/>
+          <BInputWithValidation vid="password" rules="required" type="password" icon="lock" label="Password" v-model="password" password-reveal/>
 
-              <b-field>
-                <b-button type="submit" class="is-primary my-login-button" expanded @click="handleSubmit(login)">Login</b-button>
-              </b-field>
+          <b-field>
+            <b-button type="submit" class="is-primary my-login-button" expanded @click="handleSubmit(login)">Login</b-button>
+          </b-field>
 
-              <p class="has-text-dark my-subtext">Don't have an account? <router-link to="/admin/register" class="btn-clear">Register here</router-link></p>
-            </form>
-            </ValidationObserver>
-          </section>
-        </div>
-      </div>
-    </section>
+          <p class="has-text-dark my-subtext">Don't have an account? <router-link to="/admin/register" class="btn-clear">Register here</router-link></p>
+        </form>
+        </ValidationObserver>
+      </section>      
+    </div>
   </div>
 </template>
 
@@ -91,36 +84,46 @@ export default {
 <style lang="scss" scoped>
 @import "../styles/app.scss";
 
-.my-login-hero {
-  display: grid;
-  align-content: start;
-  align-items: start;
+.my-login {
+  padding: 1rem;
 }
 
 .my-form-wrapper {
   background: #fff;
   padding: 1rem;
+}
+
+.my-form, .confirmPassword {
+  display: grid;
+  grid-gap: 15px;
+}
+
+.my-login-button {
+  margin-top: 15px;
+}
+
+@media only screen and (min-width: 700px) {
+
+  .my-login {
+    padding: 0;
+    // overflow: hidden;
+    // position: relative;
+    background-color: #ff7700;
+    background-image: url("../assets/Flat-Mountains.svg");
+    background-attachment: fixed;
+    background-size: cover;
+    height: 100%;
   }
 
-  .my-form, .confirmPassword {
-    display: grid;
-    grid-gap: 15px;
-  }
-
-  .my-login-button {
-    margin-top: 15px;
-  }
-
-@media only screen and (min-width: 600px) {
   .my-form-wrapper {
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-
     box-sizing: border-box;
     min-width: 200px;
     max-width: 500px;
     border-radius: 5px;
     color: #333;
     padding: 3rem 3rem 5rem 3rem;
+    margin-top: 100px;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
   }
 }
     
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,43 +1,35 @@
 <template>
-  <div class="">
-    <section class="welcome-content is-fullheight">
-      <img class="welcome-content-image is-hidden-mobile" src="../assets/Sun-Tornado.svg" width="100%">
-      <!-- Got this svg background from https://www.svgbackgrounds.com/#sun-tornado -->
-      
-      <!-- <span class="welcome-content-shade is-hidden-mobile"></span> -->
-      <div class="hero-body">
-        
-        <div class="container">
+  <!-- Got this svg background from https://www.svgbackgrounds.com/#sun-tornado -->
 
-          <section class="my-form-wrapper container">
-            <ValidationObserver ref="form" v-slot="{ handleSubmit }">
-              <!-- <form class="my-form container" @submit.prevent="handleSubmit(register)"> -->
-              <form class="my-form container" @keyup.enter.prevent="handleSubmit(register)">
-                <p class="title is-size-4 has-text-primary has-text-centered"><strong>Register</strong></p>
-                <BInputWithValidation vid="email" rules="required|email" type="email" icon="email" placeholder="John.smith@mail.com" label="Email" v-model="email"/>
-                <BInputWithValidation rules="required|min:2|max:30" type="text" icon="account" placeholder="John" label="First Name" v-model="firstName"/>
-                <BInputWithValidation rules="required|min:2|max:30" type="text" icon="account-group" placeholder="Smith" label="Last Name" v-model="lastName"/>
-                
-                <ValidationObserver class="confirmPassword"> 
+  <div class="my-register">
+    <div class="container">
+      <section class="my-form-wrapper container">
+        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+          <form class="my-form container" @keyup.enter.prevent="handleSubmit(register)">
+            <p class="title is-size-4 has-text-primary has-text-centered"><strong>Register</strong></p>
+            <BInputWithValidation vid="email" rules="required|email" type="email" icon="email" placeholder="John.smith@mail.com" label="Email" v-model="email"/>
+            <BInputWithValidation rules="required|min:2|max:30" type="text" icon="account" placeholder="John" label="First Name" v-model="firstName"/>
+            <BInputWithValidation rules="required|min:2|max:30" type="text" icon="account-group" placeholder="Smith" label="Last Name" v-model="lastName"/>
+            
+            <ValidationObserver class="confirmPassword"> 
 
-                  <BInputWithValidation rules="required|min:7|max:30" type="password" icon="lock" label="Password" vid="confirmation" v-model="password" password-reveal/>
-                  <BInputWithValidation rules="required|min:7|max:30|confirmed:confirmation" type="password" icon="lock"  v-model="confirmPassword"  label="Confirm Password" password-reveal/>
+              <BInputWithValidation rules="required|min:7|max:30" type="password" icon="lock" label="Password" vid="confirmation" v-model="password" password-reveal/>
+              <BInputWithValidation rules="required|min:7|max:30|confirmed:confirmation" type="password" icon="lock"  v-model="confirmPassword"  label="Confirm Password" password-reveal/>
 
-                </validationObserver>
+            </validationObserver>
 
-                <b-field>
-                  <b-button type="submit" class="is-primary my-register-button" expanded @click="handleSubmit(register)">Register</b-button>
-                </b-field>
-                <p class="has-text-dark has-text-centered">Got an account? <router-link to="/admin/login" class="btn-clear">Click here to Login</router-link></p>
-                {{errorFeedback}}
-              </form>
-            </ValidationObserver>
-
-          </section>
-        </div>
-      </div>
-    </section>
+            <b-field>
+              <b-button type="submit" class="is-primary my-register-button" expanded @click="handleSubmit(register)">Register</b-button>
+            </b-field>
+            <p class="has-text-dark has-text-centered">Got an account? <router-link to="/admin/login" class="btn-clear">Click here to Login</router-link></p>
+            {{errorFeedback}}
+          </form>
+        </ValidationObserver>
+      </section>        
+    </div>
   </div>
+
+
 </template>
 
 <script>
@@ -108,31 +100,49 @@ export default {
 <style lang="scss" scoped>
 @import "../styles/app.scss";
 
+.my-register {
+  padding: 1rem;
+}
+
+.my-form-wrapper {
+  background: #fff;
+  padding: 1rem;
+
+}
+
+.my-form, .confirmPassword {
+  display: grid;
+  grid-gap: 15px;
+}
+
+.my-register-button {
+  margin-top: 15px;
+}
+
+
+@media only screen and (min-width: 700px) {
+
+  .my-register {
+    padding: 0;
+    // overflow: hidden;
+    // position: relative;
+    background-color: #ff7700;
+    background-image: url("../assets/Sun-Tornado.svg");
+    background-attachment: fixed;
+    background-size: cover;
+    min-height: 100%;
+  }
+
   .my-form-wrapper {
-background: #fff;
-    padding: 1rem;
-
-  }
-
-  .my-form, .confirmPassword {
-    display: grid;
-    grid-gap: 15px;
-  }
-
-  .my-register-button {
-    margin-top: 30px;
-  }
-
-@media only screen and (min-width: 600px) {
-  .my-form-wrapper {
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-
     box-sizing: border-box;
     min-width: 200px;
     max-width: 500px;
     border-radius: 5px;
     color: #333;
     padding: 3rem 3rem 5rem 3rem;
+    margin-top: 100px;
+    margin-bottom: 100px;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
   }
 }
     
